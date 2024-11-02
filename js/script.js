4c77820c8e92a4c290cd56d6cfbb066bced28d65
unknown <zekryboody@gmail.com>
2024-11-02 22:10:27 +0200

    Space Eploration's Website

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Initialize all tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Initialize all popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var bott = document.querySelectorAll('.bott-al');
+
+bott.forEach(function (bott) {
+  bott.onclick = function () {
+    alert("YOUR MASSAGE HAS BEEN SENT");
+  }
+})
